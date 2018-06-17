eb475f1720247af40ebbfa501c632f7f94074b6b
AlCast <alvaro.castelan.21@gmail.com>
2018-06-17 15:53:11 -0500

    set up server and handlebars

---
+++ server.js
@@ -0,0 +1,43 @@
+//require dependencies 
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+//require scraping tools
+var axios = require("axios");
+var cheerio = require("cheerio"); 
+//require models for database 
+//var db = require("./models");
+
+//require handlebars 
+var exphbs = require("express-handlebars");
+app.set("views" , "./views");
+app.engine("handlebars",exphbs({defaultLayout:"main"}));
+app.set("view engine", ".handlebars"); 
+
+ 
+
+//logger (morgan)
+app.use(logger("dev")); 
+//body parser 
+app.use(bodyParser.urlencoded({extended: false})); 
+
+//static dir
+app.use(express.static("public"));
+
+//require routes 
+require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app); 
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+var PORT = 3010;
+app.listen(process.env.PORT || 3010, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  });
